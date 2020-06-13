096352195b593e33247dc45570e21f588a25392a
Andrew Mintah <andymintah@yahoo.com>
2020-06-13 02:08:48 +0000

    second commit

---
+++ build/precache-manifest.9e01b1e205683d5f8d9c924c3b4e6885.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0af22fb2b66b4a62d446bf26492ece10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa87c0d7c4d3b0d3e960",
+    "url": "/static/css/main.0f93a89c.chunk.css"
+  },
+  {
+    "revision": "dbf8f858c07e0ec81b99",
+    "url": "/static/js/2.92691516.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.92691516.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa87c0d7c4d3b0d3e960",
+    "url": "/static/js/main.3c64bb1f.chunk.js"
+  },
+  {
+    "revision": "3bb02338304888f7e826",
+    "url": "/static/js/runtime-main.cee7b8e8.js"
+  }
+]);
